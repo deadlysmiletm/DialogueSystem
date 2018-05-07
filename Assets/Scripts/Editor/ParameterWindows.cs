24f08ded4f1a6ac248ebe9ed9bd492c5076c20d0
guilleboc93 <32335704+guilleboc93@users.noreply.github.com>
2018-05-07 18:32:26 -0300

    Parametro script creado
    cree lo básico nada mas ya que no se muy bien que tanto vamos a poner pero tenía pensado en que se le pueda setear las textura de en donde van a aparecer los díalogos y las tipografías

---
+++ Assets/Scripts/Editor/ParameterWindows.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ParameterWindows : EditorWindow
+{
+    private int myNumberOfDialogues;
+    private int numberOfSentences;
+
+    [MenuItem("Dialogue System/Pramaters Editor")]
+    static void CreateWindow()
+    {
+        ((ParameterWindows)GetWindow(typeof(ParameterWindows))).Show();
+    }
+
+    private void OnGUI()
+    {
+        minSize = new Vector2(350,300);
+
+        GUILayout.Label("You can edit the parameter of the Dialogue System", EditorStyles.boldLabel);
+        EditorGUILayout.BeginHorizontal();
+        myNumberOfDialogues = EditorGUILayout.IntField("Number of dialogues",myNumberOfDialogues);
+        GUILayout.Button("Create");
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        numberOfSentences = EditorGUILayout.IntField("Number of sentences", numberOfSentences);
+        GUILayout.Button("Create");
+        EditorGUILayout.EndHorizontal();
+
+
+    }
+}
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Main : MonoBehaviour {
+public class Main : MonoBehaviour
+{
 
 	// Use this for initialization
 	void Start () {
